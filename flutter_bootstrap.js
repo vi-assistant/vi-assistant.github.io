0b7e9e906f09a565bbc550ffd2b2e9f41193b36f
Igbana <igbanaaondosoo13@gmail.com>
2025-11-01 17:26:00 +0100

    test: fixed somethign

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"060c2cc550a0b0d37aa48d40b5f1bb8984b743
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1233470388"
+    serviceWorkerVersion: "3023677810"
   }
 });
